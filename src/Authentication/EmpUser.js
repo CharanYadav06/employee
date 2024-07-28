f9bbd3aae093b03f772c9e9337e8248ba567e8b5
CharanYadav06 <bcharanyadav06@gmail.com>
2024-07-28 12:41:19 +0530

    sc
    m

---
+++ src/Authentication/EmpUser.js
@@ -0,0 +1,39 @@
+
+
+
+
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDCyf-kAC-15ZovLgpb3K4mG6zr7-23_QI",
+    authDomain: "employee-list-affb8.firebaseapp.com",
+    projectId: "employee-list-affb8",
+    storageBucket: "employee-list-affb8.appspot.com",
+    messagingSenderId: "338171762649",
+    appId: "1:338171762649:web:346ea2a75e5f3fa996ebc8",
+    measurementId: "G-TGJPJ3F7Z6"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth=getAuth();
+
+ 
+
+
+
+export function UserAuth() {
+  const [user, setUser] = useState(null); // Correctly define user and setUser
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (u) => setUser(u));
+    return () => unsubscribe();
+  }, []);
+
+  return user;
+}
